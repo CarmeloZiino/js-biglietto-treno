6acb12a54d79034932ab78af54bbd638546018a7
“CarmeloZiino” <carmeloziino.dev@gmail.com>
2025-01-09 13:10:26 +0100

    Creazione Repo

---
+++ js/main.js
@@ -0,0 +1,24 @@
+/* Il programma dovrà chiedere all'utente il numero di chilometri che vuole percorrere e l'età del passeggero.
+Sulla base di queste informazioni dovrà calcolare il prezzo totale del viaggio, secondo queste regole:
+- il prezzo del biglietto è definito in base ai km (0.21 € al km)
+- va applicato uno sconto del 20% per i minorenni
+- va applicato uno sconto del 40% per gli over 65.
+- L'output del prezzo finale va messo fuori in forma umana (con massimo due decimali, per indicare centesimi sul prezzo). Questo richiederà un minimo di ricerca. */
+
+
+
+
+// 1. Impostare Variabili (Nome, Cognome, Età, Chilometri da percorrere)
+
+// 2. Chiedere all'utente Nome 
+
+// 3. Chiedere all'utente Cognome
+
+// 4. Chiedere all'utente Età
+
+// 5. Chiedere all'utente Chilometri da Percorrere
+
+// 6.  Impostare il prezzo totale in base ai Km
+
+// 7. Calcolare lo Sconto {(Se < di 18 è del 20%) oppure (Se > 65 è del 40% )} e Applicarlo al prezzo totale
+
